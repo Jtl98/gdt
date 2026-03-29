fe7451ac4822b97c860fc9d8359a077366914045
josh <joshualewisvic@gmail.com>
2026-03-29 21:09:32 +1100

    add blank image download

---
+++ src/Canvas.tsx
@@ -0,0 +1,42 @@
+import { useEffect } from "react";
+import type { Rgb } from "./types";
+
+export default function Canvas({ canvasRef, newImageData }: CanvasAttributes) {
+  useEffect(() => {
+    const { current } = canvasRef;
+    const context = current?.getContext("2d");
+    if (!context || newImageData.length === 0) return;
+
+    const width = newImageData[0].length;
+    const height = newImageData.length;
+    const imageData = context.createImageData(width, height);
+    const { data } = imageData;
+
+    for (let y = 0; y < height; y++) {
+      for (let x = 0; x < width; x++) {
+        const index = (y * width + x) * 4;
+        const { r, g, b } = newImageData[y][x];
+
+        data[index] = r;
+        data[index + 1] = g;
+        data[index + 2] = b;
+        data[index + 3] = 255;
+      }
+    }
+
+    context.putImageData(imageData, 0, 0);
+  }, [canvasRef, newImageData]);
+
+  return (
+    <canvas
+      ref={canvasRef}
+      width={newImageData[0]?.length ?? 0}
+      height={newImageData.length}
+    ></canvas>
+  );
+}
+
+type CanvasAttributes = {
+  canvasRef: React.RefObject<HTMLCanvasElement | null>;
+  newImageData: Rgb[][];
+};
--- src/Textures.tsx
+++ src/Textures.tsx
@@ -1,7 +1,48 @@
+import { useMemo, useRef } from "react";
+import Canvas from "./Canvas";
+import type { Rgb } from "./types";
+
+const imageFilename = "texture.png";
+
 export default function Textures() {
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+
+  function download() {
+    const image = canvasRef.current?.toDataURL("image/png");
+    if (!image) return;
+
+    const link = document.createElement("a");
+    link.href = image;
+    link.download = imageFilename;
+    link.click();
+  }
+
   return (
     <div>
-      <h1>textures</h1>
+      <Canvas
+        canvasRef={canvasRef}
+        newImageData={useMemo(() => createWhiteImageData(), [])}
+      />
+      <br />
+      <button onClick={download}>download</button>
     </div>
   );
+}
+
+function createWhiteImageData() {
+  const width = 256;
+  const height = 256;
+  const imageData: Rgb[][] = [];
+
+  for (let y = 0; y < height; y++) {
+    const row: Rgb[] = [];
+
+    for (let x = 0; x < width; x++) {
+      row.push({ r: 255, g: 255, b: 255 });
+    }
+
+    imageData.push(row);
+  }
+
+  return imageData;
 }
