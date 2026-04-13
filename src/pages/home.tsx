4e8f3e9d787a794735895252cb33c8b4bd9a0cda
josh <joshualewisvic@gmail.com>
2026-04-13 16:57:24 +1000

    update home page with link to textures

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,9 +1,39 @@
-import { Container } from "@mui/material";
+import {
+  Card,
+  CardActionArea,
+  CardContent,
+  CardMedia,
+  Container,
+  Stack,
+  Typography,
+} from "@mui/material";
+import { Link } from "react-router";
+import whiteNoiseImage from "../assets/whiteNoiseImage.png";
 
 export default function Home() {
   return (
     <Container maxWidth="md">
-      <h1>game dev tools</h1>
+      <Typography align="center" gutterBottom variant="h2">
+        game dev tools
+      </Typography>
+
+      <Stack direction="row" justifyContent="center" marginBottom={2}>
+        <Card>
+          <CardActionArea component={Link} to="/gdt/textures">
+            <CardMedia
+              alt="white noise texture"
+              component="img"
+              image={whiteNoiseImage}
+              width={256}
+              height={256}
+            />
+
+            <CardContent>
+              <Typography variant="h6">generate textures</Typography>
+            </CardContent>
+          </CardActionArea>
+        </Card>
+      </Stack>
     </Container>
   );
 }
